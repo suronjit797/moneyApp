083571868e081134aa84b9357dab376ff794f1e9
suronjit797 <suronjit797@gmail.com>
2022-10-08 19:51:13 +0600

    server

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
 
-import Layout from '../Components/Layout'
+import Layout from '../components/Layout'
 import styles from '../styles/Home.module.css'
 import { home } from '../store/actions/authActions'
 import { connect } from 'react-redux'
 import { useEffect } from 'react'
-import TransitionList from '../Components/TransitionList'
-import RightNav from '../Components/RightNav'
+import TransitionList from '../components/TransitionList'
+import RightNav from '../components/RightNav'
 
 
 function Home({ home }) {
@@ -20,7 +20,11 @@ function Home({ home }) {
       <div className={styles.home}>
         <RightNav />
         <TransitionList />
+
       </div>
+
+
+
     </Layout>
   )
 }
