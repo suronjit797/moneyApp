e9a7564277732fb29cb45a0bc31b151f047a31ed
suronjit797 <suronjit797@gmail.com>
2022-10-08 19:08:55 +0600

    home page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,3 @@
-import dynamic from "next/dynamic";
-
 
 import Layout from '../components/Layout'
 import styles from '../styles/Home.module.css'
@@ -8,7 +6,6 @@ import { connect } from 'react-redux'
 import { useEffect } from 'react'
 import TransitionList from '../components/TransitionList'
 import RightNav from '../components/RightNav'
-// const TransitionChart = dynamic(() => import('../components/TransitionChart'), { ssr: false });
 
 
 function Home({ home }) {
@@ -23,11 +20,7 @@ function Home({ home }) {
       <div className={styles.home}>
         <RightNav />
         <TransitionList />
-
       </div>
-
-
-
     </Layout>
   )
 }
